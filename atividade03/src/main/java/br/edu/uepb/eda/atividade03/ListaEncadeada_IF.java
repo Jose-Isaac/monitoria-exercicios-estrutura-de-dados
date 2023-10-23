54cc167776f00cbe94deb3a5a62c9698a0b4ce01
Jose Isaac <joseisaacbarreirocampos@gmail.com>
2023-10-23 19:54:36 -0300

    atividade03 template

---
+++ atividade03/src/main/java/br/edu/uepb/eda/atividade03/ListaEncadeada_IF.java
@@ -0,0 +1,14 @@
+package br.edu.uepb.eda.atividade03;
+
+/*
+ * Interface para implementa��o da classe "ListaEncadeada"
+ * que deve ser implementada usando m�todos recursivos.
+ */
+public interface ListaEncadeada_IF {
+	public boolean isEmpty();
+	public int size();
+	public Integer search(Integer element) throws Exception; //lan�ar exce��o caso n�o encontre
+	public void insert(Integer element); //deve-se inserir no in�cio da lista
+	public void remove(Integer element);
+	public int[] toArray();
+}
