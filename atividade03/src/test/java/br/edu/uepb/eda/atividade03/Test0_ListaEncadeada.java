f691199c65425f2498935c97f1ce027c4d11627c
Kaio Vinícius Cordeiro Batista <104592660+kaioviniciuscb@users.noreply.github.com>
2023-10-24 02:16:27 -0300

    Update Test0_ListaEncadeada.java

--- atividade03/src/test/java/br/edu/uepb/eda/atividade03/Test0_ListaEncadeada.java
+++ atividade03/src/test/java/br/edu/uepb/eda/atividade03/Test0_ListaEncadeada.java
@@ -4,10 +4,11 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class Test0_ListaEncadeada {
 
-	private static ListaEncadeada_IF l ;
+public class Test0_ListaEncadeada {
 	
+	static ListaEncadeada_IF l ;
+		
 	@Before //Instanciando a Lista antes de cada teste
 	public void instanciaLista(){
 		l = new ListaEncadeada();
@@ -27,32 +28,6 @@ public class Test0_ListaEncadeada {
 		assertEquals(4, l.size());
 	}
 	
-	@Test
-	public void testEmpty() {
-		assertTrue(l.isEmpty());
-	}
-	
-	@Test
-	public void testSearch() throws Exception {
-		for(int i=1; i <= 5; i++){
-			l.insert(i);
-		}
-		assertEquals(3, (int) l.search(3));
-	}
-	
-	@Test
-	public void testInsert() throws Exception {
-		l.insert(1);
-		assertEquals(1, (int) l.search(1));
-	}
-	
-	@Test
-	public void testToArray() {
-		for(int i=1; i <= 5; i++){
-			l.insert(i);
-		}
-		int[] result = l.toArray();
-		assertEquals(5, result[0]);
-	}
+	//Outros testes ...
 	
 }
