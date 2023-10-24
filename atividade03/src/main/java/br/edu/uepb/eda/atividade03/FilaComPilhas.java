60fa07c7071f4883fea3df868b6bd817e556081e
Kaio Vinícius Cordeiro Batista <kaiovbatista19@gmail.com>
2023-10-24 01:14:59 -0300

    Atividade 03 feita por Eros Franklin, José Guilherme e Kaio Vinícius

---
+++ atividade03/src/main/java/br/edu/uepb/eda/atividade03/FilaComPilhas.java
@@ -0,0 +1,66 @@
+package atividade03;
+
+public class FilaComPilhas implements Fila_IF {
+
+	protected PilhaComListas p1;
+	protected PilhaComListas p2;
+
+	public FilaComPilhas() {
+		p1 = new PilhaComListas();
+		p2 = new PilhaComListas();
+	}
+
+	@Override
+	public void enqueue(Integer element) throws Exception {
+		if (p1.isFull()) {
+			throw new Exception("Fila está cheia.");
+		} else {
+			while(!p1.isEmpty()) {
+				p2.push(p1.pop());
+			}
+			p2.push(element);
+			while(!p2.isEmpty()) {
+				p1.push(p2.pop());
+			}
+		}
+	}
+
+	@Override
+	public Integer dequeue() throws Exception {
+		if (p1.isEmpty()) {
+			throw new Exception("Fila vazia.");
+		} else {
+			int dequeued = p1.pop();
+			return dequeued;
+		}
+	}
+
+	@Override
+	public Integer head() throws Exception {
+		if (p1.isEmpty()) {
+			throw new Exception("Fila vazia.");
+		} else {
+			int Head = p1.top();
+			return Head;
+		}
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if (p1.isEmpty()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean isFull() {
+		if (p1.isFull()) {
+			return false;
+		} else {
+			return false;
+		}
+	}
+
+}
---
+++ atividade03/src/main/java/br/edu/uepb/eda/atividade03/PilhaComListas.java
@@ -0,0 +1,58 @@
+package atividade03;
+
+public class PilhaComListas implements Pilha_IF {
+
+	protected ListaEncadeada l;
+
+	public PilhaComListas() {
+		this.l = new ListaEncadeada();
+	}
+
+	@Override
+	public void push(Integer element) throws Exception {
+		if (isFull()) {
+			throw new Exception("Pilha cheia.");
+		} else {
+			l.insert(element);
+		}
+	}
+
+	@Override
+	public Integer pop() throws Exception {
+		if (isEmpty()) {
+			throw new Exception("Pilha vazia");
+		} else {
+			int removed = l.data;
+			l.remove(removed);
+			return removed;
+		}
+	}
+
+	@Override
+	public Integer top() throws Exception {
+		if (isEmpty()) {
+			throw new Exception("Pilha vazia.");
+		} else {
+			return l.data;
+		}
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if (l.isEmpty()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean isFull() {
+		if (l.size() == 10) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
