eee3179239cd22a458f86b8628d345aca35cbc87
raw <rawenhyof@gmail.com>
2023-10-24 20:27:34 -0300

    Update testpilha

--- atividade03/src/test/java/br/edu/uepb/eda/atividade03/Test0_Pilha.java
+++ atividade03/src/test/java/br/edu/uepb/eda/atividade03/Test0_Pilha.java
@@ -1,6 +1,7 @@
-package br.edu.uepb.eda.atividade03;
+package atividade03;
 
 import static org.junit.Assert.*;
+
 public class Test0_Pilha {
 
     private static Pilha_IF p;
@@ -16,53 +17,4 @@ public class Test0_Pilha {
     }
 
     //Outros testes ...
-    @Test
-    public void testPushAndTop() {
-        try {
-            p.push(20);
-            assertFalse(p.isEmpty());
-            assertEquals(20, p.top(), 0);
-            assertFalse(p.isEmpty());
-        } catch (Exception e) {
-            fail("Exceção lançada: " + e.getMessage());
-        }
-    }
-
-    @Test
-    public void testIsFull() {
-        assertFalse(p.isFull());
-
-        for (int i = 0; i < 10; i++) {
-            try {
-                p.push(i);
-            } catch (Exception e) {
-                fail("Exceção lançada: " + e.getMessage());
-            }
-        }
-
-        assertTrue(p.isFull());
-    }
-
-    @Test(expected = Exception.class)
-    public void testPushWhenFull() throws Exception {
-        for (int i = 0; i < 11; i++) {
-            p.push(i);
-        }
-    }
-
-    @Test
-    public void testPushAndPopSequence() {
-        try {
-            for (int i = 1; i <= 5; i++) {
-                p.push(i);
-            }
-            for (int i = 5; i >= 1; i--) {
-                assertEquals(i, p.pop(), 0);
-            }
-            assertTrue(p.isEmpty());
-        } catch (Exception e) {
-            fail("Exceção lançada: " + e.getMessage());
-        }
-    }
-
 }
