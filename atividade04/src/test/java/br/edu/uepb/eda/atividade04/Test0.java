fffc6d6b8f9e9b96aca2db8d6f65eb637605fa14
Jose Isaac <joseisaacbarreirocampos@gmail.com>
2023-11-13 20:25:39 -0300

    add test in template

---
+++ atividade04/src/test/java/br/edu/uepb/eda/atividade04/Test0.java
@@ -0,0 +1,11 @@
+package br.edu.uepb.eda.atividade03;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Test0 {
+
+    // add your tests here or create a new class
+
+}
