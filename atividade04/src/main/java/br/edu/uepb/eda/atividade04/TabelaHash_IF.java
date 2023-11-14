29c3b1d805907a9f1a73ed7bc4df2fe65879883b
Yasmin Lima <yasmin.d.lim@gmail.com>
2023-11-14 14:18:55 -0300

    Teste 04

--- atividade04/src/main/java/br/edu/uepb/eda/atividade04/TabelaHash_IF.java
+++ atividade04/src/main/java/br/edu/uepb/eda/atividade04/TabelaHash_IF.java
@@ -6,20 +6,20 @@ package br.edu.uepb.eda.atividade04;
  */
 public interface TabelaHash_IF {
 	public void insert(Integer element);
-	public void remove(Integer element) throws Exception; //lan�ar exce��o caso o element n�o esteja na tabela
-	public int search(Integer element) throws Exception; //lan�ar exce��o caso o element n�o esteja na tabela
+	public void remove(Integer element) throws Exception; //lançar exceção caso o element não esteja na tabela
+	public Integer search(Integer element) throws Exception; //lançar exceção caso o element não esteja na tabela
 	public String print();
-	
+
 	/*
 	 OBSERVACAO:
 	 O metodo print() deve mostrar todos os elementos de cada posicao da tabela.
 	 Exemplo (com uma tabela de 8 posicoes):
 	 0: 24, 16
-	 1: 
+	 1:
 	 2: 10
 	 3: 19, 11, 3
 	 4: 12, 4
-	 5: 
+	 5:
 	 6: 22, 14, 6
 	 7: 15
 	 */
