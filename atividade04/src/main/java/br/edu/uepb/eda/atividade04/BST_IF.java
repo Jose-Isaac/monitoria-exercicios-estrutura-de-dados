82e9a61f254e650b01d1a9dfb83c0b72a4b776ed
Yasmin Lima <yasmin.d.lim@gmail.com>
2023-11-15 18:36:45 -0300

    Teste10

--- atividade04/src/main/java/br/edu/uepb/eda/atividade04/BST_IF.java
+++ atividade04/src/main/java/br/edu/uepb/eda/atividade04/BST_IF.java
@@ -1,14 +1,13 @@
 package br.edu.uepb.eda.atividade04;
 
-
 /*
  * Interface para implementação da classe "BST".
  */
 public interface BST_IF {
-    public void insert(Integer element);
-    public Integer search(Integer element) throws Exception;
-	public Integer [] preOrder();
-	public Integer [] order();
-	public Integer [] postOrder();
+	public void insert(Integer element);
+	public Integer search(Integer element) throws Exception; //lan�ar exce��o caso n�o encontre
+	public Integer[] preOrder();
+	public Integer[] order();
+	public Integer[] postOrder();
 	public boolean isComplete();
 }
