b8db1948106c723af9727f9910db9aa796af31ec
jeffgabriiel <jeffgabriiel@gmail.com>
2023-11-15 08:32:23 -0300

    :test_tube: test: Teste BTS

--- atividade04/src/main/java/br/edu/uepb/eda/atividade04/BST.java
+++ atividade04/src/main/java/br/edu/uepb/eda/atividade04/BST.java
@@ -2,145 +2,149 @@ package br.edu.uepb.eda.atividade04;
 
 import java.util.ArrayList;
 
-class No {
-    int value;
-    No left;
-    No right;
-
-    public No(int value) {
-        this.value = value;
-    }
-
-}
-
 public class BST implements BST_IF {
-    private No raiz;
+	protected Integer data;
+	protected BST left;
+	protected BST right;
+	protected BST parent;
+	
+	public BST() {
+		this.data = null;
+		this.left = null;
+		this.right = null;
+		this.parent = null;
+	}
+	
+	public boolean isEmpty() {
+		return this.data==null;
+	}
 
-    private int tamanho;
+	@Override
+	public void insert(Integer element) {
+		if (isEmpty()) {
+			this.data = element;
+		} else {
+			if(element < this.data) {
+				if (this.left == null) {
+					this.left = new BST();
+					this.left.insert(element);
+					this.left.parent = this;
+				} else {
+					this.left.insert(element);
+				}
+			} else {
+				if (this.right == null) {
+					this.right = new BST();
+					this.right.insert(element);
+					this.right.parent = this;
+				} else {
+					this.right.insert(element);
+				}
+			}
+		}
+	}
 
-    @Override
-    public void insert(Integer element) {
-        raiz = insert(raiz, element);
-        tamanho++;
+	@Override
+	public Integer search(Integer element) throws Exception {
+		if (isEmpty()) {
+			throw new Exception("BST vazia.");
+		} else {
+			if (this.data == element) {
+				return element;
+			} else if (element < this.data && this.left != null) {
+				return this.left.search(element);
+			} else if (element > this.data && this.right != null) {
+				return this.right.search(element);
+			}
+		}
+		throw new Exception("Valor não encontrado!");
+	}
+	
+	@Override
+	public int[] preOrder() {
+		ArrayList<Integer> result = new ArrayList<>();
+		preOrder_route(this, result);
+		return byArrayListToVector(result);
+	}
 
-    }
+	private void preOrder_route(BST bst, ArrayList<Integer> result) {
+		if(bst != null) {
+			result.add(bst.data);
+			preOrder_route(bst.left,result);
+			preOrder_route(bst.right,result);
+		}
+	}
 
-    private No insert(No no, int element) {
-        if (no == null) {
-            return new No(element);
+	@Override
+	public int[] order() {
+		ArrayList<Integer> result = new ArrayList<>();
+		orderRoute(this, result);
+		return byArrayListToVector(result);
+	}
+	
+	private void orderRoute(BST bst, ArrayList<Integer> result) {
+		if(bst != null) {
+			orderRoute(bst.left,result);
+			result.add(bst.data);
+			orderRoute(bst.right,result);
+		}
+	}
 
-        } else if (element < no.value) {
-            no.left = insert(no.left, element);
+	@Override
+	public int[] postOrder() {
+		ArrayList<Integer> result = new ArrayList<>();
+		postOrderRoute(this, result);
+		return byArrayListToVector(result);
+	}
+	
+	private void postOrderRoute(BST bst, ArrayList<Integer> result) {
+		if(bst != null) {
+			postOrderRoute(bst.left,result);
+			postOrderRoute(bst.right,result);
+			result.add(bst.data);
+		}
+	}
+	
+	@Override
+	public boolean isComplete() {
+		return isComplete_recursive(this, 0, heightMin(this));
+	}
 
-        } else if (element > no.value) {
-            no.right = insert(no.right, element);
-        }
+	private int heightMin(BST bst) {
+		if (bst == null) {
+			return 0;
+		} else {
+			int leftHeight = heightMin(bst.left);
+			int rightHeight = heightMin(bst.right);
 
-        return no;
+			if (leftHeight < rightHeight)
+				return leftHeight + 1;
+			else
+				return rightHeight + 1;
+		}
+	}
 
-    }
+	private boolean isComplete_recursive(BST bst, int index, int heightMin) {
+		if (bst == null && index == 0)
+			return false;
 
-    @Override
-    public Integer search(Integer element) throws Exception {
-        No no = search(raiz, element);
+		if (bst == null)
+			return true;
 
-        if (no == null) {
-            throw new Exception("Valor não encontrado");
-        }
+		if (index == (heightMin - 1))
+			return bst.left == null && bst.right == null;
 
-        return no.value;
-    }
+		else if (index > (heightMin - 1))
+			return false;
 
-    private No search(No no, int element) {
-        if (no == null || no.value == element) {
-            return no;
-
-        } else if (element < no.value) {
-            return search(no.left, element);
-
-        } else {
-            return search(no.right, element);
-
-        }
-
-    }
-
-    @Override
-    public int[] preOrder() {
-        ArrayList<Integer> lista = new ArrayList<>();
-        preOrder(raiz, lista);
-        return lista.stream().mapToInt(Integer::intValue).toArray();
-
-    }
-
-    private void preOrder(No no, ArrayList<Integer> lista) {
-        if (no != null) {
-            lista.add(no.value);
-            preOrder(no.left, lista);
-            preOrder(no.right, lista);
-        }
-
-    }
-
-    @Override
-    public int[] order() {
-        ArrayList<Integer> lista = new ArrayList<>();
-        order(raiz, lista);
-        return lista.stream().mapToInt(Integer::intValue).toArray();
-    }
-
-    private void order(No no, ArrayList<Integer> lista) {
-        if (no != null) {
-            order(no.left, lista);
-            lista.add(no.value);
-            order(no.right, lista);
-        }
-
-    }
-
-    @Override
-    public int[] postOrder() {
-        ArrayList<Integer> lista = new ArrayList<>();
-        postOrder(raiz, lista);
-        return lista.stream().mapToInt(Integer::intValue).toArray();
-
-    }
-
-    private void postOrder(No no, ArrayList<Integer> lista) {
-        if (no != null) {
-            postOrder(no.left, lista);
-            postOrder(no.right, lista);
-            lista.add(no.value);
-
-        }
-
-    }
-
-    @Override
-    public boolean isComplete() {
-        return isCompleteRec(raiz, 0, countNodes(raiz));
-
-    }
-
-    private boolean isCompleteRec(No raiz, int indice, int contador) {
-        if (raiz == null) {
-            return true;
-        }
-
-        if (indice >= contador) {
-            return false;
-        }
-
-        return isCompleteRec(raiz.left, 2 * indice + 1, contador) &&
-                isCompleteRec(raiz.right, 2 * indice + 2, contador);
-    }
-
-    private int countNodes(No raiz) {
-        if (raiz == null) {
-            return 0;
-        }
-
-        return 1 + countNodes(raiz.left) + countNodes(raiz.right);
-    }
-
+		return isComplete_recursive(bst.left, index + 1, heightMin) && isComplete_recursive(bst.right, index + 1, heightMin);
+	}
+	
+	private int[] byArrayListToVector(ArrayList<Integer> list) {
+		int vector[] = new int[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			vector[i] = list.get(i);
+		}
+		return vector;
+	}
 }
