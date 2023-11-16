3772e36322a1720555dad4048740fce9d85e5f9f
Arthur Franco <pedro.franco@aluno.uepb.edu.br>
2023-11-16 14:51:14 -0300

    Update BST_IF.java

--- atividade04/src/main/java/br/edu/uepb/eda/atividade04/BST_IF.java
+++ atividade04/src/main/java/br/edu/uepb/eda/atividade04/BST_IF.java
@@ -5,9 +5,14 @@ package br.edu.uepb.eda.atividade04;
  */
 public interface BST_IF {
     public void insert(Integer element);
-    public Integer search(Integer element) throws Exception; //lançar exceção caso não encontre
+
+    public Integer search(Integer element) throws Exception; // lançar exceção caso não encontre
+
     public Integer[] preOrder();
+
     public Integer[] order();
+
     public Integer[] postOrder();
+
     public boolean isComplete();
 }
