5524ae9319288221e2c2df56606a920d96bb9973
Arthur Franco <pedro.franco@aluno.uepb.edu.br>
2023-11-16 14:56:49 -0300

    Update BST_IF.java

--- atividade04/src/main/java/br/edu/uepb/eda/atividade04/BST_IF.java
+++ atividade04/src/main/java/br/edu/uepb/eda/atividade04/BST_IF.java
@@ -4,15 +4,11 @@ package br.edu.uepb.eda.atividade04;
  * Interface para implementação da classe "BST".
  */
 public interface BST_IF {
-    public void insert(Integer element);
-
-    public Integer search(Integer element) throws Exception; // lançar exceção caso não encontre
-
-    public Integer[] preOrder();
-
-    public Integer[] order();
-
-    public Integer[] postOrder();
-
-    public boolean isComplete();
+	public void insert(Integer element);
+	public Integer search(Integer element) throws Exception; //lan�ar exce��o caso n�o encontre
+	public Integer[] preOrder();
+	public Integer[] order();
+	public Integer[] postOrder();
+	public boolean isComplete();
+}
 }
